87691a29b2e2e1da9c6132f5780a4445a11707f1
Adriano Gomes de Oliveira <oliveradriano.ads@gmail.com>
2022-02-09 22:26:59 -0300

    método obteralunos no controller alunosController criado

--- backend/AlunosApi/AlunosApi/Controllers/AlunosController.cs
+++ backend/AlunosApi/AlunosApi/Controllers/AlunosController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AlunosApi.Models;
+using AlunosApi.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,10 +13,25 @@ namespace AlunosApi.Controllers
     [ApiController]
     public class AlunosController : ControllerBase
     {
-        private readonly IAlunosservice
-        public AlunosController()
-        {
+        private readonly IAlunoService _alunoService;
 
+        public AlunosController(IAlunoService alunoService)
+        {
+            _alunoService = alunoService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IAsyncEnumerable<Aluno>>> ObterAlunos()
+        {
+            try
+            {
+                var alunos = await _alunoService.ObterAlunos();
+                return Ok(alunos);
+            }
+            catch 
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao obter lista de alunos");
+            }
         }
     }
 }
